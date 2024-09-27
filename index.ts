66a4e2b7bb1e858df7ae6753e78c1d125d03acb0
Suresh Kumar Jeeva <jsureshkumarj1@gmail.com>
2024-09-27 21:27:41 +0530

    completed the api implementation

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -7,12 +8,16 @@ app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/api/add", (req, res)=>{
-    res.json([{
-        data:req.body
-    }]);
-});
+app.use(bodyParser.raw({inflate:true, limit: '100kb', type: 'application/json'}));
+app.use(express.json());
 
+app.post("/api/add", (req, res)=>{
+    let jsonData = JSON.parse(req.body);
+    console.log(jsonData);
+    let result = add(jsonData.numberstring);
+    console.log(result);
+    res.json([{"result":result}]);
+});
 
 app.listen(8080, ()=>{
     console.log("Server running at https://localhost:8080");
@@ -23,9 +28,13 @@ function add(numbers:String): Number{
     let negative = false;
     let delimiter = ",";
     let negativeNumbers = Array();
+    numbers = numbers.substring(0);
+    console.log("numbers:"+numbers);
     if(numbers.substring(0,2) == "//"){
         delimiter = numbers.substring(2,numbers.indexOf("\n"));
+        console.log("delimiter:"+delimiter);
         numbers = numbers.substring(numbers.indexOf("\n") + 1);
+        console.log("numbers:"+numbers);
     }
     numbers.split(delimiter).forEach(element => {
         let internalArray = element.split("\n");
