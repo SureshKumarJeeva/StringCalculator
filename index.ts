0d1b0dd3cca46bcc273578092f1591d9fee9d399
Suresh Kumar Jeeva <jsureshkumarj1@gmail.com>
2024-09-27 17:43:46 +0530

    Backend functionality implemented

--- index.ts
+++ index.ts
@@ -1,28 +1,36 @@
-function add(numbers:String): Number | never{
+import http from "http";
+
+export const server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-type': "text/html"});
+    res.end(
+        //JSON.stringify("data: It works")
+        
+    );
+});
+
+server.listen(3000, ()=>{
+    console.log("Server running at https://localhost:3000");
+});
+
+function add(numbers:String): Number{
     let sum = 0;
     let negative = false;
     let delimiter = ",";
     let negativeNumbers = Array();
     if(numbers.substring(0,2) == "//"){
-        console.log("indexOf(\n):"+numbers.indexOf("\n"));
         delimiter = numbers.substring(2,numbers.indexOf("\n"));
         numbers = numbers.substring(numbers.indexOf("\n") + 1);
     }
-    console.log("delimiter:"+delimiter);
-    console.log("numbers:"+numbers);
     numbers.split(delimiter).forEach(element => {
-        console.log(element);
-        console.log(element.split("\n"));
-        console.log(element.indexOf("\n"));
-            let internalArray = element.split("\n");
-            internalArray.forEach(subelement =>{
-                if(Number(subelement) < 0){
-                    negativeNumbers.push(subelement);
-                    negative = true;
-                }
-                if(!negative)
-                    sum += Number(subelement);
-            });
+        let internalArray = element.split("\n");
+        internalArray.forEach(subelement =>{
+            if(Number(subelement) < 0){
+                negativeNumbers.push(subelement);
+                negative = true;
+            }
+            if(!negative)
+                sum += Number(subelement);
+        });
     });
     if (!negative)
         return sum;
