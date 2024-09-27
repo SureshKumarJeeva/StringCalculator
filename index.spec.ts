17bc644d68e16b795fc94ac2a7b7088614ccafdf
Suresh Kumar Jeeva <jsureshkumarj1@gmail.com>
2024-09-27 12:41:36 +0530

    Assertion complete for dynamic delimiter

--- index.spec.ts
+++ index.spec.ts
@@ -1,6 +1,8 @@
 import add from "./index";
 
 test("should sum string of numbers delimited by an operator", ()=>{
+    expect(add("")).toBe(0);
     expect(add("1,2")).toBe(3);
     expect(add("1,2,4")).toBe(7);
+    expect(add("1\n2,4")).toBe(7);
 });
--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 function add(numbers:String): Number{
     let sum = 0;
     numbers.split(",").forEach(element => {
-        sum += Number(element);
+        console.log(element);
+        console.log(element.split("\n"));
+        console.log(element.indexOf("\n"));
+        if(element.indexOf("\n") > -1){
+            let internalArray = element.split("\n");
+            console.log(internalArray);
+            sum += Number(internalArray[0]) + Number(internalArray[1]);
+        }else{
+            sum += Number(element);
+        }
     });
     return sum;
 }
